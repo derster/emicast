a453bc5498cf99fe4c0ca69b404a85d04459c3f0
Modeste KOUADIO <modstk1@gmail.com>
2015-12-09 18:36:46 +0000

    Modifier routes sign_up et sign_in en registration et login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :admins
 
+  as :admin do
+  	get "/login" => "devise/sessions#new"
+
+  	get "/registration" => "devise/registrations#new"
+  end
+
   root 'welcome#index'
 
   resources :emissions
