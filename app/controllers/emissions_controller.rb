e1ae37a57304ebbfdf94c5de93a859efd4ab6b9f
Modeste KOUADIO <modstk1@gmail.com>
2016-01-13 22:26:29 +0000

    Ajout de css hover aux box de page d'accueil et section inscription radio

--- app/controllers/emissions_controller.rb
+++ app/controllers/emissions_controller.rb
@@ -5,7 +5,12 @@ class EmissionsController < ApplicationController
   	before_filter :require_permission, only: [:edit]
 	
 	def index
-	  	@emissions = Emission.all.order("created_at DESC")
+	  	if params[:category].blank?
+	      @emissions = Emission.all.order("created_at DESC")
+	    else
+	      @category_id = Category.find_by(name: params[:category]).id
+	      @emissions = Emission.where(:category_id => @category_id).order("created_at DESC")
+	    end
 	end
 
 	def search
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 		@episodes = Episode.all.order("created_at DESC").limit(8)
+		@emissions = Emission.all.order("created_at DESC").limit(4)
 	end 
 
 	def episodes
